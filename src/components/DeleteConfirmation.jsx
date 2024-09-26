e6e0eb54eaf0b1f5cc7a1a45968eea223c085cc5
Mark Fasel <markfasel@gmail.com>
2024-09-26 13:29:27 -0400

    use callback

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,16 +1,27 @@
+import { useEffect } from 'react';
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  return (
-    <div id="delete-confirmation">
-      <h2>Are you sure?</h2>
-      <p>Do you really want to remove this place?</p>
-      <div id="confirmation-actions">
-        <button onClick={onCancel} className="button-text">
-          No
-        </button>
-        <button onClick={onConfirm} className="button">
-          Yes
-        </button>
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            onConfirm();
+        }, 3000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [onConfirm]);
+
+    return (
+        <div id="delete-confirmation">
+            <h2>Are you sure?</h2>
+            <p>Do you really want to remove this place?</p>
+            <div id="confirmation-actions">
+                <button onClick={onCancel} className="button-text">
+                    No
+                </button>
+                <button onClick={onConfirm} className="button">
+                    Yes
+                </button>
+            </div>
+        </div>
+    );
 }
